7f7bceb96a9a118299959f128b4d760115a7c0a9
Atul <atul.ak1510@gmail.com>
2022-10-08 22:35:16 +0530

    Added Reverse Pairs Problem

---
+++ C++/Reverse Pairs.cpp
@@ -0,0 +1,19 @@
+// Problem Link : https://leetcode.com/problems/reverse-pairs/
+
+#include<bits/stdc++.h>
+using namespace std;
+ int reversePairs(vector < int > & arr) {
+      int Pairs = 0;
+      for (int i = 0; i < arr.size(); i++) {
+        for (int j = i + 1; j < arr.size(); j++) {
+          if (arr[i] > 2 * arr[j]) Pairs++;
+        }
+      }
+      return Pairs;
+    }
+
+
+int main()
+{
+    vector<int> arr{1,3,2,3,1};
+    cout<<"The Total Reverse Pairs are "<<reversePairs(arr);
